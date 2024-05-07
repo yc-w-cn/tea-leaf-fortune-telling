fe774b5d6fbb6209a86c54024c2a460ee79b9c56
YC.W <contact@wangyuchen.cn>
2024-05-07 18:36:25 +0800

    chore: update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: isProduction ? "export" : "standalone",
+  basePath: isProduction ? "/tea-leaf-fortune-telling" : undefined,
+  assetPrefix: isProduction ? "/tea-leaf-fortune-telling" : undefined,
+};
 
 export default nextConfig;
