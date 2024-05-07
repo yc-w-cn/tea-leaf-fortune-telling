14f45861e89504db3d76c1ef97016f3d92615850
YC.W <contact@wangyuchen.cn>
2024-05-07 19:00:21 +0800

    chore: add gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const isProduction = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: isProduction ? "export" : "standalone",
+  basePath: isProduction ? "/tea-leaf-fortune-telling" : undefined,
+  assetPrefix: isProduction ? "/tea-leaf-fortune-telling" : undefined,
 };
 
 export default nextConfig;
